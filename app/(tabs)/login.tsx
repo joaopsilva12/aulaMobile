93bb23763e4aad4f5f88e11bbe61a8ab419abc10
Joao Paulo De Arruda E Silva - FATEC - Cuiaba/MT <joao.parruda@alunosenai.mt>
2025-10-28 21:42:08 -0400

    Mudança na pagina de login

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,15 +1,70 @@
-import { View, StyleSheet, Text} from 'react-native';
+import { View, StyleSheet, Text, Button, TextInput } from 'react-native';
+
+import Toast from 'react-native-toast-message';
+
+import { useState } from 'react';
 
 export default function Login(){
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const autenticar = async () => {
+        Toast.show({
+            type: 'success',
+            text1: 'Sucesso',
+            text2: 'Login efetuado!'
+        })
+    }
+
     return(
-        <View>
+        <View style={ style.container }>
+            <Toast />
             <Text>
-                Só Alegria!
+                Login no Sistema
             </Text>
+
+            <TextInput
+                placeholder='Email'
+                value={ email }
+                onChangeText={ setEmail }
+            />
+
+            <TextInput
+                placeholder='Senha'
+                value={ senha }
+                onChangeText={ setSenha }
+            />
+
+            <Button
+                title="Login"
+                onPress={ autenticar }
+            />
+
         </View>
     )
 }
 
 const style = StyleSheet.create({
-
+    container:{
+        backgroundColor: "#fff",
+        flex: 1,
+        justifyContent: 'center',
+        padding: 10,
+    },
+    
+    titulo: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#eee8eff',
+        marginBottom: 15,
+    },
+    input: {
+        borderColor: "gray",
+        borderStyle: 'solid',
+        borderRadius: 10,
+        padding: 10,
+        marginBottom: 10,
+    }
 });
